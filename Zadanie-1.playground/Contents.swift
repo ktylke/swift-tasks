75bf9322636967c2138cbb8e240251d48a7fd2bb
ktylke <k.tylke7@gmail.com>
2016-12-22 20:33:07 +0100

    message

---
+++ Zadanie-1.playground/Contents.swift
@@ -0,0 +1,39 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+class Zwierzak {
+    var name: String
+    var species: String
+    var picture: UIImage?
+    
+    func isThereAPicture() -> String {     //determines if there is a picture
+        
+        if picture != nil {
+        
+            return "Posiada zdjęcie"
+          
+        } else {
+            
+            return "Nie posiada zdjęcia"
+        }
+    }
+        
+
+    func description() -> String {
+        
+        return name + " - " + species + " - " + isThereAPicture()
+    }
+    
+    init(name: String, species: String, picture: UIImage?) {
+        self.name = name
+        self.species = species
+        self.picture = picture
+    }
+}
+
+var Sanic = Zwierzak(name: "Sanic", species: "The hedgehog", picture: nil)
+
+Sanic.description()
